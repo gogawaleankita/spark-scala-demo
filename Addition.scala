c5e8f9fbb4bd2c2fdb8129aedaaefb60ecacfdfa
gogawaleankita <gogawaleankita33@gmail.com>
2021-10-10 23:51:40 +0530

    basic additon OP

---
+++ Addition.scala
@@ -0,0 +1,18 @@
+// println("val and var demo");
+var a:Int=12;
+var b:Int =13;
+println("addition"+(a+b));
+val a:Int=12;
+println("new val :: "+a);
+val `my.string` ="java";
+
+/* output
+Loading Addition.scala...
+val and var demo
+a: Int = 12
+b: Int = 13
+addition25
+a: Int = 12
+new val :: 12
+my.string: String = java
+*/
